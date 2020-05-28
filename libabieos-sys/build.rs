3a5d8c0184367efbb155c4af70c16d31338d9623
Ian Holsman <ian@holsman.org>
2020-05-28 10:13:00 -0400

    update abieos, build off abieos .. not abieos_static

--- libabieos-sys/build.rs
+++ libabieos-sys/build.rs
@@ -5,12 +5,12 @@
 fn main() {
     let dst = cmake::Config::new("lib/abieos")
       //  .build_target("abieos_static")
-       .build_target("abieos_static")
+       .build_target("abieos")
         .build();
     // let dst = cmake::build("lib/abieos");
     println!("cargo:rustc-link-lib=c++");
     println!("cargo:rustc-link-search={}/build", dst.display());
-    println!("cargo:rustc-link-lib=abieos_static");
+    println!("cargo:rustc-link-lib=abieos");
     //println!("cargo:rustc-link-lib=stdc++");
 
 
